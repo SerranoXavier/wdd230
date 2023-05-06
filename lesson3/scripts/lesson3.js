aef9ffb27eb0ba0f6725441c2b7bbc3863daa111
Xavier Serrano <xav.serrano@yahoo.fr>
2023-05-06 05:07:29 -0400

    Lesson 3: Plan for the Chamber of Commerce site

---
+++ lesson3/scripts/lesson3.js
@@ -0,0 +1,37 @@
+// Current Year
+const date = new Date();
+const year = date.getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
+// Last Modified
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const dateLastModif = new Date(document.lastModified);
+const dayLastModif = dateLastModif.getDate();
+const monthLastModif = months[dateLastModif.getMonth()];
+const yearLastModif = dateLastModif.getFullYear();
+const hourLastModif = dateLastModif.getHours();
+const minuteLastModif = dateLastModif.getMinutes();
+document.querySelector("#lastmodified").textContent = `${dayLastModif} ${monthLastModif} ${yearLastModif} ${hourLastModif}:${minuteLastModif}`;
+
