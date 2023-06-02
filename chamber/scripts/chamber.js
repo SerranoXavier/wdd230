13cb47ba5546780bcfbcbf83272e801cfa36dc91
Xavier Serrano <xav.serrano@yahoo.fr>
2023-06-01 23:07:11 -0400

    Chamber: wind chill: separate JS file + else statement

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -58,11 +58,3 @@ if (dayBannerDisplayed.includes(d.getDay())) {
 	document.querySelector("#banner").style.display = "block";
 }
 
-
-// wind chill factor
-const temp = document.querySelector("#temp").textContent;
-const windSpeed = document.querySelector("#windSpeed").textContent;
-if (temp <= 10 && windSpeed >= 4.8) {
-	const windChill = 13.2 + (0.6215 * temp) - (11.37 * windSpeed ** 0.16) + (0.3965 * temp * windSpeed ** 0.16);
-	document.querySelector("#windChill").textContent = Math.round(windChill) + " °C";
-}
---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,10 @@
+// wind chill factor
+const temp = document.querySelector("#temp").textContent;
+const windSpeed = document.querySelector("#windSpeed").textContent;
+if (temp <= 10 && windSpeed >= 4.8) {
+	const windChill = 13.2 + (0.6215 * temp) - (11.37 * windSpeed ** 0.16) + (0.3965 * temp * windSpeed ** 0.16);
+	document.querySelector("#windChill").textContent = Math.round(windChill) + " °C";
+}
+else {
+	document.querySelector("#windChill").textContent = "N/A"
+}
