a1c04509c73d1d577fd73548b06d3d12897911d8
Xavier Serrano <xav.serrano@yahoo.fr>
2023-06-23 17:47:14 -0400

    Week 10: Chamber of Commerce - Weather -  img src

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -3,40 +3,44 @@ function titleCase(string) {
 	// lower case all the characters
 	string = string.toLowerCase();
 	// split the string into arrays of words
-	string = string.split(" ");
+	string = string.split(' ');
 	// map the array replacing the first letter of each word by upper case
 	string = string.map(function(word) {
 		return word.replace(word[0], word[0].toUpperCase());
 	});
 	// join the words of the array
-	return string.join(" ");
+	return string.join(' ');
 };
 
 function displayResults(weatherData) {
-    const temp = weatherData.main.temp;
-    const iconSrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-	const iconDesc = weatherData.weather[0].description;
-    const speed = weatherData.wind.speed;
-    const humidityValue = weatherData.main.humidity;
-    const pressureValue = weatherData.main.pressure;
+    const temp = weatherData.main.temp; // defines the temperature from the API data
+    const iconSrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`; // defines the img source from the API data
+	const iconDesc = weatherData.weather[0].description; // defines the img description from the API data
+    const speed = weatherData.wind.speed; // defines the wind speed from the API data
+    const humidityValue = weatherData.main.humidity; // defines the humidity from the API data
+    const pressureValue = weatherData.main.pressure; // defines the pressure from the API data
+	const weatherIcon = document.createElement('img'); // creates an img element
+	weatherIcon.setAttribute('id', 'weatherIcon'); // set the id of the img
+	const captionDesc = document.createElement('figcaption'); // creates a figcaption element
 
-	currentTemp.innerHTML = `<mark>${temp.toFixed(0)}</mark> °C` ;
+	currentTemp.innerHTML = `<mark>${temp.toFixed(0)}</mark> °C`; // set the innerHTML of the temperature
 
-	weatherIcon.setAttribute('src', iconSrc);
-	weatherIcon.setAttribute('alt', iconDesc);
-	captionDesc.textContent = titleCase(iconDesc);
+	weatherFigure.appendChild(weatherIcon); // append the img to the figure element
+	weatherFigure.appendChild(captionDesc); // append the figcaption to the figure element
+	weatherIcon.setAttribute('src', iconSrc); // set the src of the img
+	weatherIcon.setAttribute('alt', iconDesc); // set the alt of the img
+	captionDesc.textContent = titleCase(iconDesc); // set the content of the figcaption
 
-    windSpeedElement.innerHTML = `<mark>${speed.toFixed(0)}</mark> km/h`;
+    windSpeedElement.innerHTML = `<mark>${speed.toFixed(0)}</mark> km/h`; // set the innerHTML of the wind speed
 
-    humidity.innerHTML = `<mark>${humidityValue}</mark> %`;
-    pressure.innerHTML = `<mark>${pressureValue}</mark> hPa`;
+    humidity.innerHTML = `<mark>${humidityValue}</mark> %`; // set the innerHTML of the humidity
+    pressure.innerHTML = `<mark>${pressureValue}</mark> hPa`; // set the innerHTML of the pressure
 };
 
 
 // select HTML elements in the document
 const currentTemp = document.querySelector('#temp');
-const weatherIcon = document.querySelector('#weatherIcon');
-const captionDesc = document.querySelector('#weather figcaption');
+const weatherFigure = document.querySelector('#weatherFigure');
 const windSpeedElement = document.querySelector('#windSpeed');
 const humidity = document.querySelector('#humidity');
 const pressure = document.querySelector('#pressure');
