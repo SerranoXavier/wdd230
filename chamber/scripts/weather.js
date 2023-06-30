2838c8100a3881fac5e3506d766b254466b55e98
Xavier Serrano <xav.serrano@yahoo.fr>
2023-06-29 23:10:09 -0400

    Week 11: Chamber of Commerce - windchill factor: same script than weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -69,3 +69,15 @@ async function apiFetch() {
 }
   
 apiFetch();
+
+// wind chill factor
+
+const temp = document.querySelector("#temp").textContent;
+const windSpeed = document.querySelector("#windSpeed").textContent;
+if (temp <= 10 && windSpeed >= 4.8) {
+	const windChill = 13.2 + (0.6215 * temp) - (11.37 * windSpeed ** 0.16) + (0.3965 * temp * windSpeed ** 0.16);
+	document.querySelector("#windChill").innerHTML = `<mark>${Math.round(windChill)}</mark> °C`;
+}
+else {
+	document.querySelector("#windChill").textContent = "N/A"
+}
